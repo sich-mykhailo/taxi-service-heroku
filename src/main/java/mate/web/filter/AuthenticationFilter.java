3690c3f8c56006d1160a1854be71161a50a5e756
MykhailoSich <sichmisha@gmail.com>
2021-10-20 12:47:33 +0300

    initial commit

---
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -0,0 +1,35 @@
+package mate.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AuthenticationFilter implements Filter {
+    private static final String SESSION_ATTRIBUTE_ID = "driver_id";
+    private Set<String> allowedUrl;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        allowedUrl = new HashSet<>();
+        allowedUrl.add("/login");
+        allowedUrl.add("/drivers/add");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        Long driverId = (Long) session.getAttribute(SESSION_ATTRIBUTE_ID);
+        if (driverId == null && !allowedUrl.contains(req.getServletPath())) {
+            resp.sendRedirect("/login");
+            return;
+        }
+        chain.doFilter(req, resp);
+    }
+}
